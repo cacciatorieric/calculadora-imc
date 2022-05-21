c0a0f96db1069e7ba4f3d837870c415ecfe456da
Eric Cacciatori Bellini <eric.cacciatori@hotmail.com>
2022-05-21 19:40:45 -0300

    mudança nome splash

--- calculadora_imc/lib/screens/splash_screen.dart
+++ calculadora_imc/lib/screens/splash_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
